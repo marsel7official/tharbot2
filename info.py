1665e34c76c9a03e1bc516bae4e4a9d9aa362d3d
MarselDeveloper <141441864+marsel7official@users.noreply.github.com>
2024-03-02 07:50:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,9 +48,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "RIONETWORKS")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'pdiskpro.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1ca1fe6ebb0523de51228b0e84195d15746ad6af')
+VERIFY = bool(environ.get('VERIFY', False))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onepagelink.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '1fb1e56aa57d893927b06afea819b4f3419b282d)
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
